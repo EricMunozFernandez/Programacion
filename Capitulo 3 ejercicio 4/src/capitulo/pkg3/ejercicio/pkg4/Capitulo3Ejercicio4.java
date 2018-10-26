d6a37c1d7f1d49ddaf74ef877e1ebedbd60c430a
Eric <eric.munoz@ikasle.egibide.org>
2018-10-26 12:44:18 +0200

    terminado con catch

--- Capitulo 3 ejercicio 4/src/capitulo/pkg3/ejercicio/pkg4/Capitulo3Ejercicio4.java
+++ Capitulo 3 ejercicio 4/src/capitulo/pkg3/ejercicio/pkg4/Capitulo3Ejercicio4.java
@@ -21,22 +21,40 @@ public class Capitulo3Ejercicio4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try {
-            // TODO code application logic here
-            String fechaString = JOptionPane.showInputDialog ("Teclea una fecha dd/MM/yy");
-            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yy");
-            Date fechaUno=formatter.parse(fechaString);
-            fechaString=JOptionPane.showInputDialog ("Teclea una fecha dd/MM/yy");
-            SimpleDateFormat formatterDos = new SimpleDateFormat ("dd/MM/yy");
-            Date fechaDos=formatterDos.parse(fechaString);
-            long milisegundos=fechaUno.getTime()-fechaDos.getTime();
-            long dias=milisegundos/86400000;
-            JOptionPane.showMessageDialog(null,"La diferencia de las dos fechas es de "+dias+" dias");
-        } catch (ParseException ex) 
+        boolean error=false;
+        do
+        {
+        try 
             {
-                Logger.getLogger(Capitulo3Ejercicio4.class.getName()).log(Level.SEVERE, null, ex);
+                // TODO code application logic here
+                //para calendar es Calendar fecha Uno=Calendar.getInstance(); antes de todo lo mismo que con Date
+                //para localDate DateTimeFormatter formatter=DateTimeFormatter.ofPattern("dd/MM/yy");
+                String fechaString = JOptionPane.showInputDialog ("Teclea una fecha dd/MM/yy");
+                SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yy");
+                Date fechaUno=formatter.parse(fechaString);
+                //Para Calendar fechaUno.setTime(formatter.parse(fechaString);
+                //Para LocalDate LocalDate fechaUno=LocalDate.parse(fechaString,formatter);
+                fechaString=JOptionPane.showInputDialog ("Teclea una fecha dd/MM/yy");
+                SimpleDateFormat formatterDos = new SimpleDateFormat ("dd/MM/yy");
+                Date fechaDos=formatterDos.parse(fechaString);
+                long milisegundos=fechaUno.getTime()-fechaDos.getTime();
+                //En Calendar es getTimeInMillis()
+                //Para LocalDate Period period=Period.between(fechaUno,fechaDos);
+                long dias=milisegundos/86400000;
+                JOptionPane.showMessageDialog(null,"La diferencia de las dos fechas es de "+dias+" dias");
+                error=false;
+            } 
+        catch (ParseException ex) 
+            {
+                JOptionPane.showMessageDialog(null,"Ya te vale");
+                error=true;
             }
-
+        finally
+            {
+                JOptionPane.showMessageDialog(null,"Se acabo");
+            }
+        }
+        while(error==true);
     }
     
 }
