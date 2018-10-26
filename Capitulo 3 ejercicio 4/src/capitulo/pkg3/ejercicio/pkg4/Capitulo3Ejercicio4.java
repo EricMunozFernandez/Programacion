3bea765f2301538cb1d4279cd207fb112aaba039
Eric <eric.munoz@ikasle.egibide.org>
2018-10-26 13:35:04 +0200

    4 definitivo y 5 sin revisar

--- Capitulo 3 ejercicio 4/src/capitulo/pkg3/ejercicio/pkg4/Capitulo3Ejercicio4.java
+++ Capitulo 3 ejercicio 4/src/capitulo/pkg3/ejercicio/pkg4/Capitulo3Ejercicio4.java
@@ -25,18 +25,18 @@ public class Capitulo3Ejercicio4 {
         try 
             {
                 // TODO code application logic here
-                //para calendar es Calendar fecha Uno=Calendar.getInstance(); antes de todo lo mismo que con Date
+                //para calendar es Calendar fechaUno=Calendar.getInstance(); antes de todo lo mismo que con Date
                 //para localDate DateTimeFormatter formatter=DateTimeFormatter.ofPattern("dd/MM/yy");
                 String fechaString = JOptionPane.showInputDialog ("Teclea una fecha dd/MM/yy");
                 SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yy");
                 Date fechaUno=formatter.parse(fechaString);
-                //Para Calendar fechaUno.setTime(formatter.parse(fechaString);
+                //Para Calendar fechaUno.setTime(formatter.parse(fechaString));
                 //Para LocalDate LocalDate fechaUno=LocalDate.parse(fechaString,formatter);
                 fechaString=JOptionPane.showInputDialog ("Teclea una fecha dd/MM/yy");
                 SimpleDateFormat formatterDos = new SimpleDateFormat ("dd/MM/yy");
                 Date fechaDos=formatterDos.parse(fechaString);
                 long milisegundos=fechaUno.getTime()-fechaDos.getTime();
-                //En Calendar es getTimeInMillis()
+                //En Calendar es getTimeInMillis();
                 //Para LocalDate Period period=Period.between(fechaUno,fechaDos);
                 long dias=milisegundos/86400000;
                 JOptionPane.showMessageDialog(null,"La diferencia de las dos fechas es de "+dias+" dias");
---
+++ capitulo 3 ejercicio 5/src/capitulo/pkg3/ejercicio/pkg5/Capitulo3Ejercicio5.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capitulo.pkg3.ejercicio.pkg5;
+
+/**
+ *
+ * @author 1gdaw07
+ */
+import javax.swing.JOptionPane;
+import java.util.Date;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+public class Capitulo3Ejercicio5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean error=false;
+        do
+        {
+            try
+            {
+                Calendar fechaUno = Calendar.getInstance();                
+                String FechaString=JOptionPane.showInputDialog("inserta la fecha dd/MM/yy");                
+                SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yy");
+                fechaUno.setTime(formatter.parse(FechaString));
+                fechaUno.add(Calendar.DAY_OF_YEAR,100);                
+                JOptionPane.showMessageDialog(null,"La fecha es "+fechaUno.getTime());
+                error=false;
+            }
+            catch(Exception e)
+            {
+                JOptionPane.showMessageDialog(null,"Error");
+                error=true;
+            }
+            finally
+            {
+                JOptionPane.showMessageDialog(null,"Programa finalizado");
+            }
+        }
+        while(error==true);
+    }
+    
+}
