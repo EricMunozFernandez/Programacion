ea5c238f1efde0605b7dc6e0ed496272c0969966
Eric <eric.munoz@ikasle.egibide.org>
2018-11-05 13:23:15 +0100

    Revert "3.3.3 terminado"
    This reverts commit 9fad87a88b8e552871978bc651d2b85083101059.

--- Capitulo 3 Practica 3 Ejercicio 3/src/capitulo/pkg3/practica/pkg3/ejercicio/pkg3/Capitulo3Practica3Ejercicio3.java
+++ Capitulo 3 Practica 3 Ejercicio 3/src/capitulo/pkg3/practica/pkg3/ejercicio/pkg3/Capitulo3Practica3Ejercicio3.java
@@ -18,72 +18,64 @@ public class Capitulo3Practica3Ejercicio3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        try
+        boolean error=false;
+        do
         {
-            boolean error=false;
-            do
+            try
             {
-                try
-                {                   
-                    int Opcion=Integer.parseInt(JOptionPane.showInputDialog("nº1 Sumar dos numeros. \nnº2 Restar dos numeros. \nnº3 Visualizar la tabla de multiplicar de un numero. \nnº4 Visualizar el cociente y el resto de una division. \nnº5 Salir del programa."));                   
-                        switch(Opcion)
-                        {
-                            case 1:
-                            {
-                                suma();
-                                error=true;
-                                break;
-                            }
-                            case 2:
-                            {
-                                resta();
-                                error=true;
-                                break;
-                            }
-                            case 3:
-                            {
-                                multi();
-                                error=true;
-                                break;
-                            } 
-                            case 4:
-                            {
-                                division();
-                                error=true;
-                                break;
-                            }
-                            case 5:
-                            {
-                                error=false;
-                                break;
-                            }
-                        }                                          
-                }
-                catch(NumberFormatException e)
+                int Opcion=Integer.parseInt(JOptionPane.showInputDialog("nº1 Sumar dos numeros. \nnº2 Restar dos numeros. \nnº3 Visualizar la tabla de multiplicar de un numero. \nnº4 Visualizar el cociente y el resto de una division. \nnº5 Salir del programa."));
+                switch(Opcion)
                 {
-                    JOptionPane.showMessageDialog(null,"Escribe un numero");
-                    error=true;
+                    case 1:
+                    {
+                        suma();
+                        break;
+                    }
+                    case 2:
+                    {
+                        resta();
+                        break;
+                    }
+                    case 3:
+                    {
+                        multi();
+                        break;
+                    } 
+                    case 4:
+                    {
+                        
+                        break;
+                    }
+                    case 5:
+                    {
+                        error=false;
+                        break;
+                    }
                 }
-                catch(Exception e)
-                {
-                    JOptionPane.showMessageDialog(null,"Error");
-                    error=true;
-                }
-            
+                error=false;   
             }
-            while(error==true);
-        }
-        finally
+            catch(NumberFormatException e)
+            {
+                JOptionPane.showMessageDialog(null,"La nota es un numero");   
+            }
+            catch(Exception e)
+            {
+                JOptionPane.showMessageDialog(null,"Error");
+                error=true;
+            }
+            finally
             {
                 JOptionPane.showMessageDialog(null,"FIN");
             }
+        }
+        while(error==true);
     }
     public static void suma() throws Exception
     {
         int n1=Integer.parseInt(JOptionPane.showInputDialog("inserta el numero 1 para sumar"));
         int n2=Integer.parseInt(JOptionPane.showInputDialog("inserta el numero 2 para sumar"));
         int suma=n1+n2;
-        JOptionPane.showMessageDialog(null,"La suma es "+suma);        
+        JOptionPane.showMessageDialog(null,"La suma es "+suma);
     }
     public static void resta() throws Exception
     {
@@ -95,23 +87,16 @@ public class Capitulo3Practica3Ejercicio3 {
     public static void multi() throws Exception
     {
         int numero=Integer.parseInt(JOptionPane.showInputDialog("introduce el numero a multiplicar"));
-        int c=0,multi;
-        String serie=" ";
-        while (c<11)
+        int b=1,c=0,multi;
+        String serie=numero+" "+b+" ";
+        while (c<10)
         {
             multi=numero*c;
-            serie=serie+multi+" ";            
+            serie=serie+multi+" ";
+            numero=multi;
+            b++;
             c++;
         }
         JOptionPane.showMessageDialog(null,serie);
-    }
-    public static void division() throws Exception
-    {
-        int numero=Integer.parseInt(JOptionPane.showInputDialog("introduce el numero a dividir"));
-        int divisor=Integer.parseInt(JOptionPane.showInputDialog("introduce el numero divisor"));
-        int resto=0, div;
-        div=numero/divisor;
-        resto=numero%divisor;
-        JOptionPane.showMessageDialog(null,"la division es "+div+" el resto es "+resto);
     }
 }
