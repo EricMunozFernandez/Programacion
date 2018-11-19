4bedabdba52de4379bc96e1832439ee76000a717
Eric <eric.munoz@ikasle.egibide.org>
2018-11-19 14:07:53 +0100

    4.1.5 por terminar bien

--- Tema 4/Capitulo 4 Practica 1 Ejercicio 4/src/capitulo/pkg4/practica/pkg1/ejercicio/pkg4/Capitulo4Practica1Ejercicio4.java
+++ Tema 4/Capitulo 4 Practica 1 Ejercicio 4/src/capitulo/pkg4/practica/pkg1/ejercicio/pkg4/Capitulo4Practica1Ejercicio4.java
@@ -15,7 +15,7 @@ public class Capitulo4Practica1Ejercicio4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here       
     }
     
 }
--- Tema 4/Capitulo 4 Practica 1 Ejercicio 5/src/capitulo/pkg4/practica/pkg1/ejercicio/pkg5/Capitulo4Practica1Ejercicio5.java
+++ Tema 4/Capitulo 4 Practica 1 Ejercicio 5/src/capitulo/pkg4/practica/pkg1/ejercicio/pkg5/Capitulo4Practica1Ejercicio5.java
@@ -5,6 +5,8 @@
  */
 package capitulo.pkg4.practica.pkg1.ejercicio.pkg5;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author 1gdaw07
@@ -16,6 +18,33 @@ public class Capitulo4Practica1Ejercicio5 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+        double precio=Double.parseDouble(JOptionPane.showInputDialog("Inserta precio"));       
+        double entrada=Double.parseDouble(JOptionPane.showInputDialog("Cuanto dinero tienes?"));       
+        double devolver=entrada-precio;
+        devolver=(double) Math.round(devolver * 100) / 100;
+        JOptionPane.showMessageDialog(null,"La cantidad ha devolver es"+devolver);
+        int contador[]=new int[15];
+        double dinero[]=new double[15];
+        dinero[0]=500.00;
+        dinero[1]=200.00;
+        dinero[2]=100.00;
+        dinero[3]=50.00;
+        dinero[4]=20.00;
+        dinero[5]=10.00;
+        dinero[6]=5.00;
+        dinero[7]=2.00;
+        dinero[8]=1.00;
+        dinero[9]=0.50;
+        dinero[10]=0.20;
+        dinero[11]=0.10;
+        dinero[12]=0.05;
+        dinero[13]=0.02;
+        dinero[14]=0.01;        
+        String serie="";
+        for(int x=0;x<dinero.length;x++)
+        {
+            serie=serie+contador[x]+" de "+dinero[x]+" €\n";
+        }
+        JOptionPane.showMessageDialog(null,"La cantida a devolver es:\n"+serie);       
+    }    
 }
