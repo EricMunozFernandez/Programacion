db67d767b2f0e2e6e55bb6630f9e04dc479ed380
Eric <eric.munoz@ikasle.egibide.org>
2018-11-24 16:08:39 +0100

    4.1.9 creo que he entendido bien el ejercicio como hacerlo

--- Tema 4/Capitulo 4 Practica 1 Ejercicio 9/src/capitulo/pkg4/practica/pkg1/ejercicio/pkg9/Capitulo4Practica1Ejercicio9.java
+++ Tema 4/Capitulo 4 Practica 1 Ejercicio 9/src/capitulo/pkg4/practica/pkg1/ejercicio/pkg9/Capitulo4Practica1Ejercicio9.java
@@ -5,6 +5,8 @@
  */
 package capitulo.pkg4.practica.pkg1.ejercicio.pkg9;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Usuario
@@ -16,6 +18,34 @@ public class Capitulo4Practica1Ejercicio9 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String alumno[][]={{"ingles","frances","aleman","ruso"},{ "basico","medio","perfeccionamiento"}};
+        int x;
+        String idioma=JOptionPane.showInputDialog("Introduce el idioma").toLowerCase();
+        for( x = 0; x < alumno.length &&  alumno[x].equals(idioma); x++)
+            {}
+            if (x == alumno.length)
+            {
+                JOptionPane.showMessageDialog(null,"idioma no valido");
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null,"idioma correcto");
+                String nivel =JOptionPane.showInputDialog(null, "Introduzca el nivel").toLowerCase();
+                int y;
+                for( y = 0; y < alumno[x].length &&  alumno[x].equals(nivel); y++)
+                {}
+                if (y == alumno[x].length)
+                {
+                    JOptionPane.showMessageDialog(null,"nivel no valido");
+                }
+                else
+                {
+                    JOptionPane.showMessageDialog(null,"nivel correcto");
+                    int dato = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduzca la cantidad de alumnos"));
+                    String datoString=dato+"";
+                    alumno[x][y]=datoString;
+                }
+            }
     }
     
 }
