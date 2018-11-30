e8f29d548d334fb115f25dfab4a9f321f16e24cd
Eric <eric.munoz@ikasle.egibide.org>
2018-11-30 13:24:30 +0100

    5.1.1 finalizado

--- Tema 5/Capitulo 5 Practica 1 Ejercicio 1/src/capitulo/pkg5/practica/pkg1/ejercicio/pkg1/Capitulo5Practica1Ejercicio1.java
+++ Tema 5/Capitulo 5 Practica 1 Ejercicio 1/src/capitulo/pkg5/practica/pkg1/ejercicio/pkg1/Capitulo5Practica1Ejercicio1.java
@@ -6,6 +6,7 @@
 package capitulo.pkg5.practica.pkg1.ejercicio.pkg1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import javax.swing.JOptionPane;
 
 /**
@@ -17,33 +18,66 @@ public class Capitulo5Practica1Ejercicio1 {
     /**
      * @param args the command line arguments
      */
-    public static ArrayList<Double>lista=new ArrayList();
+    public static ArrayList<Double>lista;
     public static void main(String[] args) {
         // TODO code application logic here
-        int respuesta;        
-        do
+         
+        lista=new ArrayList();      
+        try
         {
-            double dato=Double.parseDouble(JOptionPane.showInputDialog("Intorduce un numero"));
-            lista.add(dato);
-            respuesta=JOptionPane.showConfirmDialog(null,"Quieres continuar?");
+            hacerArray();
+            menu(); 
         }
-        while(respuesta==0);
+        catch(NumMalEx e)
+        {
+            JOptionPane.showMessageDialog(null,"Escribe un numero entre 1 y 10");
+        }
+        catch(NumberFormatException e)
+        {
+            JOptionPane.showMessageDialog(null,"Escribe un numero");
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null,"Error tipo "+e.getMessage());
+        }                     
+    }
+    /*public static void minimo(ArrayList<Integer>lista)*/
+    public static void hacerArray() throws Exception
+    {
+        int respuesta;
+        do
+        {           
+            pedirDato();
+            respuesta=JOptionPane.showConfirmDialog(null,"Quieres continuar?");            
+        }
+        while(respuesta==0); 
+    }
+    public static void pedirDato()
+    {
+        double dato=Double.parseDouble(JOptionPane.showInputDialog("Intorduce un numero"));
+        lista.add(dato);
+    }
+    public static void menu() throws Exception
+    {
         int opc;
         do
         {  
             opc=Integer.parseInt(JOptionPane.showInputDialog("Elige una pocion:\n1. Visualizar el valor maximo y el minimo.\n" +
-"2. Solicitar un numero y buscarlo. Muestra un mensaje indicando si lo ´\n" +
-"has encontrado o no.\n" +
-"3. Solicitar un numero, buscarlo y borrarlo. Sino se encuentra muestra ´\n" +
-"un mensaje de error.\n" +
-"4. Convertir el arrayList en un array.\n" +
-"5. Si no esta vacio, mostrar el numero de elementos que contiene. ´\n" +
-"6. Insertar un nuevo elemento por el final.\n" +
-"7. Insertar un nuevo elemento en la posicion que te indique el usuario. ´\n" +
-"8. Borrar un elemento de una posicion concreta. ´\n" +
-"9. Calcular la suma y la media aritmetica de los valores contenidos. ´\n" +
-"10. Finalizar."));
-
+            "2. Solicitar un numero y buscarlo. Muestra un mensaje indicando si lo \n" +
+            "has encontrado o no.\n" +
+            "3. Solicitar un numero, buscarlo y borrarlo. Sino se encuentra muestra \n" +
+            "un mensaje de error.\n" +
+            "4. Convertir el arrayList en un array.\n" +
+            "5. Si no esta vacio, mostrar el numero de elementos que contiene. \n" +
+            "6. Insertar un nuevo elemento por el final.\n" +
+            "7. Insertar un nuevo elemento en la posicion que te indique el usuario. \n" +
+            "8. Borrar un elemento de una posicion concreta. \n" +
+            "9. Calcular la suma y la media aritmetica de los valores contenidos. \n" +
+            "10. Finalizar."));
+            if(opc<1||opc>10)
+            {
+                throw new NumMalEx();
+            }
             switch(opc)
             {
                 case 1:
@@ -99,8 +133,6 @@ public class Capitulo5Practica1Ejercicio1 {
         }
         while(opc!=10);
     }
-
-    /*public static void minimo(ArrayList<Integer>lista)*/
     public static void minimo()
     {
         double min=1000000000;
@@ -118,7 +150,7 @@ public class Capitulo5Practica1Ejercicio1 {
         }
         JOptionPane.showMessageDialog(null,"El numero minimo es: "+min+"\nEl numero maximo es: "+max);
     }
-    public static void search()
+    public static void search() throws Exception
     {
         double numero=Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero a buscar"));
         boolean existe=lista.contains(numero);
@@ -131,7 +163,7 @@ public class Capitulo5Practica1Ejercicio1 {
             JOptionPane.showMessageDialog(null, "El numero "+numero+" no existe");
         }
     }
-    public static void searchAndDestroy()
+    public static void searchAndDestroy() throws Exception
     {
         double numero=Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero a buscar"));
         boolean existe=lista.contains(numero);
@@ -148,28 +180,29 @@ public class Capitulo5Practica1Ejercicio1 {
     public static void convertir()
     {
         Object[]listaArray=lista.toArray();
-
+        JOptionPane.showMessageDialog(null,"El arrayList se ha convertido en array: "+Arrays.toString(listaArray));
     }
     public static void mostrarNumero()
     {
-        int x;
-        for(x=0;x<lista.size()&& !lista.isEmpty();x++)
-        {}
         if(lista.isEmpty())
         {
             JOptionPane.showMessageDialog(null,"Esta vacio");
         }
         else
         {
-            JOptionPane.showMessageDialog(null,"el primer numero que hay en el array es"+lista.get(x));
+            String datos="";
+            for(Double elemento:lista)
+                datos+=elemento+" ";
+            //datos = lista.stream().map((elemento) -> elemento+" ").reduce(datos, String::concat); opcion reciente
+            JOptionPane.showMessageDialog(null,"La lista es: "+datos);
         }
     }
-    public static void insertar()
+    public static void insertar() throws Exception
     {
-        double dato=Double.parseDouble(JOptionPane.showInputDialog("Intorduce un numero"));
-        lista.add(dato);
+        pedirDato();
+        JOptionPane.showMessageDialog(null,"Has insertado el numero por el final del ArrayList");
     }
-    public static void insertarPosicion()
+    public static void insertarPosicion() throws Exception
     {
         int pos=Integer.parseInt(JOptionPane.showInputDialog("Elige la posicion en el que insertaras el numero"));
         double num=Double.parseDouble(JOptionPane.showInputDialog("Inserta el numero"));
