b32fa56bc6bbf7e034574603a4b0090e505fb0cc
Eric <eric.munoz@ikasle.egibide.org>
2018-12-15 20:21:39 +0100

    6.2.2 intento
    no se como pasar la listaPeliculas al estudio en las que el estudio a trabajado

---
+++ Tema 6/Capitulo 6 Practica 2 Ejercicio 2/src/UML/Estudio.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UML;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author 1gdaw07
+ */
+public class Estudio {
+    private String nombre;
+    private String ciudad;
+    private String direccion;
+    private String dirWeb;
+    private Date fechaFundacion;//mejor LocalDate
+    private String pais;
+    private ArrayList<String> telefono;
+    private ArrayList<Pelicula>listaPeliculas;
+    ////este es el metodo nuevoEstudio (Constructor)
+    public Estudio() {
+    }
+
+    public Estudio(String nombre, String ciudad, String direccion, String dirWeb, Date fechaFundacion, String pais, ArrayList<String> telefono, ArrayList<Pelicula> listaPeliculas) {
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.direccion = direccion;
+        this.dirWeb = dirWeb;
+        this.fechaFundacion = fechaFundacion;
+        this.pais = pais;
+        this.telefono = telefono;
+        this.listaPeliculas = listaPeliculas;
+    }
+
+    public ArrayList<Pelicula> getListaPeliculas() {
+        return listaPeliculas;
+    }
+
+    public void setListaPeliculas(ArrayList<Pelicula> listaPeliculas) {
+        this.listaPeliculas = listaPeliculas;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDirWeb() {
+        return dirWeb;
+    }
+
+    public void setDirWeb(String dirWeb) {
+        this.dirWeb = dirWeb;
+    }
+
+    public Date getFechaFundacion() {
+        return fechaFundacion;
+    }
+
+    public void setFechaFundacion(Date fechaFundacion) {
+        this.fechaFundacion = fechaFundacion;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public List getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(ArrayList telefono) {
+        this.telefono = telefono;
+    }
+    
+    public Estudio[]desplegarEstudio()
+    {
+        return null;
+    }
+    public void modificarEstudio()
+    {
+        
+    }
+    public void cierraEstudio()
+    {
+        
+    }
+}
---
+++ Tema 6/Capitulo 6 Practica 2 Ejercicio 2/src/capitulo/pkg6/practica/pkg2/ejercicio/pkg2/Capitulo6Practica2Ejercicio2.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capitulo.pkg6.practica.pkg2.ejercicio.pkg2;
+
+import UML.Estudio;
+import UML.Pelicula;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Capitulo6Practica2Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static Estudio[]listaEstudios;
+    private static ArrayList<Pelicula>listaPeliculas;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try
+        {
+        crearPeliculas();
+        crearEstudios();        
+        buscarPeliculaMasLarga();
+        buscarEstudioConMasPeliculas();
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }        
+    }
+    public static void crearPeliculas()
+    {   
+        listaPeliculas=new ArrayList();
+        ArrayList<Estudio>lista=new ArrayList();
+        lista.add(listaEstudios[1]);
+        lista.add(listaEstudios[0]);
+        char[]año=new char[4];
+        año[0]='1';
+        año[1]='9';
+        año[2]='6';
+        año[3]='0';
+        Pelicula p=new Pelicula("Terminator",año,123f,"accion",lista);
+        listaPeliculas.add(p);        
+      
+        ArrayList<Estudio>lista2=new ArrayList();
+        lista.add(listaEstudios[0]);
+        lista.add(listaEstudios[2]);
+        char[]año2=new char[4];
+        año[0]='1';
+        año[1]='9';
+        año[2]='4';
+        año[3]='8';
+        Pelicula p2=new Pelicula("ET",año2,160f,"terror",lista2);
+        listaPeliculas.add(p2);
+        
+        ArrayList<Estudio>lista3=new ArrayList();
+        lista.add(listaEstudios[0]);        
+        char[]año3=new char[4];
+        año[0]='1';
+        año[1]='9';
+        año[2]='8';
+        año[3]='4';
+        Pelicula p3=new Pelicula("yo robot",año3,100f,"comedia",lista3);
+        listaPeliculas.add(p3);
+        
+        ArrayList<Estudio>lista4=new ArrayList();
+        lista.add(listaEstudios[0]);
+        lista.add(listaEstudios[1]);
+        lista.add(listaEstudios[2]);
+        char[]año4=new char[4];
+        año[0]='1';
+        año[1]='9';
+        año[2]='2';
+        año[3]='3';
+        Pelicula p4=new Pelicula("star wars",año4,200f,"infantil",lista4);
+        listaPeliculas.add(p4);
+    }
+    public static void crearEstudios()throws Exception
+    {
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+        listaEstudios=new Estudio[3]; 
+        ArrayList<String>telefono0=new ArrayList();
+        telefono0.add("1111111111");
+        telefono0.add("222222222222");
+        listaEstudios[0]= new Estudio("Disney","LA","c/manolo","www.hola.com",sdf.parse("1994-03-12"),"EEUU",telefono0,listaPeliculas.);
+        ArrayList<String>telefono1=new ArrayList();
+        telefono1.add("35346346457");
+        telefono1.add("234343242342");
+        listaEstudios[1]= new Estudio("marvel","rivavellosa","c/juna","www.epa.com",sdf.parse("1984-07-24"),"antartida",telefono1,);
+        ArrayList<String>telefono2=new ArrayList();
+        telefono2.add("333333333333");
+        telefono2.add("4444444444444");
+        listaEstudios[2]= new Estudio("Serzulu","moon","c/soya","www.mepicaelpie.com",sdf.parse("1840-07-05"),"himalaya",telefono2);    
+    }    
+    public static void buscarPeliculaMasLarga()
+    {
+        float max=0;
+        int pos=0;
+        for(int x=0;x<listaPeliculas.size();x++)
+        {
+            max=listaPeliculas.get(x).getDuracion();
+            pos=x;
+        }
+        JOptionPane.showMessageDialog(null,"La pelicula mas larga es "+listaPeliculas.get(pos).getTitulo()+" con una duracion de "+listaPeliculas.get(pos).getDuracion());
+    }   
+    public static void buscarEstudioConMasPeliculas()
+    {
+       
+        for(int x=0;x<listaPeliculas.size();x++)
+        {
+          for(int y=0;y<listaEstudios.length;y++)
+          {
+              if(listaPeliculas.get(x).getListaEstudios().contains(listaEstudios[y]))
+              {
+                //hacer algo pero no se como ponerlo
+              }
+          }
+        }
+    }
