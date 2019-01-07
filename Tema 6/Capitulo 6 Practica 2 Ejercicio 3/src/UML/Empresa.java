59f58ce880aeea51b0f6616e3591afd2bf770908
Eric <eric.munoz@ikasle.egibide.org>
2019-01-07 14:03:04 +0100

    resubido

--- Tema 6/Capitulo 6 Practica 2 Ejercicio 3/src/UML/Empresa.java
+++ Tema 6/Capitulo 6 Practica 2 Ejercicio 3/src/UML/Empresa.java
@@ -5,11 +5,48 @@
  */
 package UML;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Usuario
  */
 public class Empresa {
     private String nombre;
+    private ArrayList<Cliente> listaClientes;
+    private ArrayList<Empleado> listaEmpleados;
+
+    public Empresa() {
+    }
+
+    public Empresa(String nombre, ArrayList<Cliente> listaClientes, ArrayList<Empleado> listaEmpleados) {
+        this.nombre = nombre;
+        this.listaClientes = listaClientes;
+        this.listaEmpleados = listaEmpleados;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Cliente> getListaClientes() {
+        return listaClientes;
+    }
+
+    public void setListaClientes(ArrayList<Cliente> listaClientes) {
+        this.listaClientes = listaClientes;
+    }
+
+    public ArrayList<Empleado> getListaEmpleados() {
+        return listaEmpleados;
+    }
+
+    public void setListaEmpleados(ArrayList<Empleado> listaEmpleados) {
+        this.listaEmpleados = listaEmpleados;
+    }
     
 }
