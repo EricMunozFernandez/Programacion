be9c2774ea83217985fb43c1545082999ba9cccd
Eric <eric.munoz@ikasle.egibide.org>
2019-01-14 14:08:44 +0100

    tema 8

---
+++ Tema 8/Ejemple ventanas/EjemploVentana/src/ejemploventana/EjemploVentana.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploventana;
+
+import vista.ventana1;
+
+/**
+ *
+ * @author 1gdaw07
+ */
+public class EjemploVentana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ventana1 v=new ventana1();
+        v.setVisible(true);
+    }
+    
+}
